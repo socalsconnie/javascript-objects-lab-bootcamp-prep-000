508f5b0fc1af3edffe96f7187d842eb3eb8f9982
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-04-25 20:49:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object);
-  
+  delete newObject[key]
+  return newObject
 }
